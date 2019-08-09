3b322fb6e70ee0f7d9387bbac18caf052cfc183e
davi ribeiro <luminuszz43@gmail.com>
2019-08-09 19:32:07 -0300

    .

--- src/server.js
+++ src/server.js
@@ -5,16 +5,20 @@ const express = require('express');
 
 // Mongoose para fazer a conexão com o banco e tambem fazer a sintaxe js ser entendida para a lingaugem do banco no caso mongodb
 const mongoose = require('mongoose');
+// Importando cors para a API ser acessada remotamente paro o REACT
+const cors = require('cors');
+
 
 // Routes do express para trabalhar com rotas
 const routes = require('./routes');
+
 
 
 // Conexão com banco utilizando mongoose 
 const server = express();
 mongoose.connect('mongodb+srv://luminuszz:7418529630@serverbank-ec2rj.mongodb.net/server_api?retryWrites=true&w=majority', 
 {useNewUrlParser: true}); // obejto de configuração do moongose para admitir a nova sintaxe
-
+server.use(cors());
 server.use(express.json()); // Fazer o express entender o arquivo do tipo JSON
 server.use(routes); // Fazer o servidor entender as rotas
 
